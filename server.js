51b9a16cedabf94a6aea577135f018ded8e0a4e4
yparam98 <yathavanparamesh1998@gmail.com>
2021-06-09 20:05:19 -0400

    added error handling

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const service = require('./data-service.js');
 const fs = require('fs');
 const { request } = require('http');
 const { response } = require('express');
+const { rejects } = require('assert');
 // const build = require('./build.js');
 
 app.listen(port, () => {
@@ -21,48 +22,48 @@ app.get('/', (request, response) => {
 
 // get comic by number
 app.get('/:num', (request, response) => {
-    response.sendFile(path.join(__dirname, '/views/comic.html'));    
+    response.sendFile(path.join(__dirname, '/views/comic.html'));
 });
 
 app.get('/api/getXKCD', (request, response) => {
     let comic_object = {};
     let error = null;
 
-    if (request.query.num == null) {
-        service.getCurrentXKCD().then((data) => {
-            comic_object = {
-                "num": data.num,
-                "title": data.safe_title,
-                "link": data.img,
-                "date": momentjs().year(data.year).month(data.month).date(data.day).format('LL')
-            };
-        }).catch((err) => {
-            comic_object = null;
-            error = err;
-        });
-    } else {
-        service.getOldXKCD(request.query.num).then((data) => {
-            comic_object = {
-                "num": data.num,
-                "title": data.safe_title,
-                "link": data.img,
-                "date": momentjs().year(data.year).month(data.month).date(data.day).format('LL')
-            };
-        }).catch((err) => {
-            comic_object = null;
-            error = err;
-        });
-    }
+    try {
+        if (request.query.num == null) {
+            service.getCurrentXKCD().then((data) => {
+                comic_object = {
+                    "num": data.num,
+                    "title": data.safe_title,
+                    "link": data.img,
+                    "date": momentjs().year(data.year).month(data.month).date(data.day).format('LL')
+                };
+            }).catch((err) => {
+                throw err;
+            });
+        } else {
+            service.getOldXKCD(request.query.num).then((data) => {
+                comic_object = {
+                    "num": data.num,
+                    "title": data.safe_title,
+                    "link": data.img,
+                    "date": momentjs().year(data.year).month(data.month).date(data.day).format('LL')
+                };
+            }).catch((err) => {
+                throw err;
+            });
+        }
 
-    fs.appendFile(path.join(__dirname, '/counter.dat'), comic_object.num); // write the comic num to file
+        fs.appendFile(path.join(__dirname, '/counter.dat'), comic_object.num); // write the comic num to file
 
-    if (comic_object != null) {
         response.send(comic_object);
-    } else {
-        response.status(500).send(error);
-    }    
+    } catch (err) {
+        response.status(500).send(err);
+    }
 });
 
 app.get('/api/getViews', (request, response) => {
-    
+    fs.readFile(path.join(__dirname, '/counter.dat'), (err, data) => {
+        console.log(data);
+    }); // write the comic num to file
 });
